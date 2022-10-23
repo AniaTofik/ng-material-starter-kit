3316eacde0651cf78cfb4447d271f5ade2ed501a
AniaTofik <anna.kiedel@onet.pl>
2022-10-23 17:02:25 +0200

    feat: Exercise 2 - Create employee form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 import { ProductFormComponentModule } from './components/product-form/product-form.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
+import { EmployeeFormComponentModule } from './components/employee-form/employee-form.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'create-product', component: ProductFormComponent }]), ProductFormComponentModule, ProductsServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'create-product', component: ProductFormComponent }, { path: 'create-employee', component: EmployeeFormComponent }]), ProductFormComponentModule, ProductsServiceModule, EmployeeFormComponentModule, EmployeeServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

038a8f8154d39573207a90d2b0f9d65fac1abbaa
AniaTofik <anna.kiedel@onet.pl>
2022-10-23 16:39:10 +0200

    feat: Exercise 1 - Create product form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ProductFormComponentModule } from './components/product-form/product-form.component-module';
+import { ProductsServiceModule } from './services/products.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{ path: 'create-product', component: ProductFormComponent }]), ProductFormComponentModule, ProductsServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
